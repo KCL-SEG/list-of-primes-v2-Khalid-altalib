0ccb6d24d35ab185a1e4afa056eedadb824641f2
Khalid-altalib <66210571+Khalid-altalib@users.noreply.github.com>
2022-12-06 19:00:58 +0000

    Full Solution

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    list = [] #list of primes
+    i = 2
+    if number_of_primes <= 0:
+        raise ValueError("number_of_primes must be a positive integer")
+    else:
+        while len(list) < number_of_primes: # while the list is less than the number of primes wanted
+            for j in range(2,i): # for every number between 2 and i
+                if i%j == 0: # if i is divisible by j
+                    break
+            else:
+                list.append(i) # if i is not divisible by any number between 2 and i (its a prime), add it to the list
+            i += 1 # increment i
+        return list
